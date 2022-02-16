a20b66766e69c4b2661b285da5c75ba94cd23966
RicBar10 <rbarrigaponcedeleon@gmail.com>
2022-02-16 22:29:49 +0100

    up to date

--- 11_04/js/singleGallery.js
+++ 11_04/js/singleGallery.js
@@ -1,5 +1,5 @@
 
-window.addEventListener('load', () => {
+/*window.addEventListener('load', () => {
     let galleriesRaw = '{"gal1": {"title": "Bretagne 2019", "images": ["image-1.jpg","image-2.jpg","image-3.jpg","image-4.jpg","image-5.jpg","image-6.jpg"]},"gal2":{ "title": "Sacramento 2017", "images": ["image-7.jpg","image-8.jpg","image-9.jpg","image-10.jpg","image-11.jpg","image-12.jpg","image-13.jpg","image-14.jpg"]}}';
     let galleries = JSON.parse(galleriesRaw);
     let searchParams = new URLSearchParams(window.location.search);
@@ -39,3 +39,4 @@ function closeSinglePict(){
     let imageContainer = document.getElementById('galleryContainer');
     imageContainer.classList.toggle('visible');
 }
+*/
