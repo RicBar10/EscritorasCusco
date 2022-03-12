b69b6b7631aaa79dac7331b4156c03a1e008c14e
Lucia Fajardo <luciafalu.2509@gmail.com>
2022-03-12 17:12:26 +0100

    html y lectura de datos de la base

--- escritoras_cusco/site_web/models.py
+++ escritoras_cusco/site_web/models.py
@@ -1,21 +1,25 @@
 from django.db import models
 
 TIPO_CATEGORIA = [
-		('Es','Escritora'),
-		('Pt','Poeta'),
-		('Ed','Editora'),
-        ('Pr','Profesora'),
-		('An','Antropologa'),
-		('Il','Ilustradora'),
+    ('Es', 'Escritora'),
+    ('Pt', 'Poeta'),
+    ('Ed', 'Editora'),
+    ('Pr', 'Profesora'),
+    ('An', 'Antropologa'),
+    ('Il', 'Ilustradora'),
 ]
 
 # Create your models here.
+
+
 class Escritora(models.Model):
     nombre = models.CharField(max_length=200)
     lugar_nacimiento = models.CharField(max_length=200)
     fecha_nacimiento = models.DateField()
-    fecha_defuncion = models.DateField()
+    fecha_defuncion = models.DateField(null=True)
     trayector = models.CharField(max_length=200)
+    link_imagen = models.CharField(max_length=200)
+    is_shown = models.BooleanField()
 
 
 class Publicacion(models.Model):
@@ -24,6 +28,7 @@ class Publicacion(models.Model):
     descripcion = models.CharField(max_length=200)
     fecha = models.DateField()
 
+
 class Categoria(models.Model):
     escritora = models.ForeignKey(Escritora, on_delete=models.CASCADE)
-    categorizacion = models.CharField(choices = TIPO_CATEGORIA, max_length=2)
+    categorizacion = models.CharField(choices=TIPO_CATEGORIA, max_length=2)
