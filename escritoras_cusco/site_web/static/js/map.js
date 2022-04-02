691906e3b52ba6e3c3dc63877d9f9642e5308186
RicBar10 <rbarrigaponcedeleon@gmail.com>
2022-04-02 09:46:16 +0200

    nuevos cambios

--- escritoras_cusco/site_web/static/js/map.js
+++ escritoras_cusco/site_web/static/js/map.js
@@ -1,5 +1,5 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var map = L.map('leafletMap').setView([-13.5363516, -72.526677, 9], 8);
+document.addEventListener('DOMContentLoaded', function() {
+	var map = L.map('leafletMap').setView([-13.5363516,-72.526677,9], 8);
 
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -10,6 +10,34 @@ document.addEventListener('DOMContentLoaded', function () {
         accessToken: 'pk.eyJ1IjoiZGllZ29zZW1pbmFyaW8iLCJhIjoiY2t5aGxvYzkyMjRpeDJ4bzgxd3RqemF0ZiJ9.HxjXDv1JRipE8aYwCQfKPA'
     }).addTo(map);
 
-    var marker = L.marker([-13.5498695, -71.953328, 12]).addTo(map);
+    var marker = L.marker([-13.5498695,-71.953328,12]).addTo(map);
 
-}); 
+    var marker2 = L.marker([-12,-68]).addTo(map);
+
+    var popup = L.popup();
+
+    var photoImg = '<a href="https://www.google.com"><img src="../img/Escritora_01.jpg" height="1200px" width="1200px"/></a>';
+
+    function onMarkerMouseOver(e) {
+        popup
+            .setLatLng(e.latlng)
+            .setContent("<center>Escritora </center>" + "</br>"+ photoImg + "</br>")
+            .openOn(map);
+    }
+
+    function onMarkerMouseClick(e) {
+        popup
+            .setLatLng(e.latlng)
+            .setContent("<center>Escritora </center>" + "</br>"+ photoImg + "</br>")
+            .openOn(map);
+    }
+
+    function onMarkerMouseOut(e) {
+        map.closePopup();
+    }
+
+    marker.on('click', onMarkerMouseClick);
+
+    marker2.on('click', onMarkerMouseClick);
+
+});
