669a09248f272bd1ca247978fe869a8042e43c74
diegopanepo <diegomv1997@hotmail.com>
2022-04-08 01:05:27 +0200

    segunda version de los script python y sql

--- escritoras_cusco/datos.sql
+++ escritoras_cusco/datos.sql
@@ -3,59 +3,63 @@ DELETE FROM site_web_publicacion;
 DELETE FROM site_web_lugar;
 DELETE FROM site_web_ejerce;
 
-INSERT INTO site_web_mujer VALUES (1,"Iñakapalla","Chávez Bermúdez",1,"20/06/1986",NULL,NULL,NULL);
-INSERT INTO site_web_mujer VALUES (2,"Rosa Augusta","Rivero Ricalde",1,"14/04/1907","01/10/1966",NULL,NULL);
-INSERT INTO site_web_mujer VALUES (3,"Inés Qorich'aska","Quispe Puma",2,"1990",NULL,NULL,NULL);
-INSERT INTO site_web_mujer VALUES (4,"Aleyda","Cárdenas",NULL,"1987",NULL,NULL,NULL);
-INSERT INTO site_web_mujer VALUES (5,"Silvia Rosa","Huamaní Quispe",3,"1982",NULL,NULL,NULL);
-INSERT INTO site_web_mujer VALUES (6,"Gladys","Conde Camargo",1,"1982",NULL,NULL,NULL);
-INSERT INTO site_web_mujer VALUES (7,"Ana María","Milla Hurtado",1,"1977",NULL,NULL,NULL);
-INSERT INTO site_web_mujer VALUES (8,"Ch'aska Eugenia","Anka Ninawaman",3,"1973",NULL,NULL,NULL);
-INSERT INTO site_web_mujer VALUES (9,"Tania","Castro",1,"1973",NULL,NULL,NULL);
-INSERT INTO site_web_mujer VALUES (10,"Karina","Pacheco Medrano",1,"1969",NULL,NULL,NULL);
-INSERT INTO site_web_mujer VALUES (11,"Carmen","Escalante Gutiérrez",1,"1953",NULL,NULL,NULL);
-INSERT INTO site_web_mujer VALUES (12,"Ana Bertha","Vizcarra",1,"1947",NULL,NULL,NULL);
-INSERT INTO site_web_mujer VALUES (13,"Mercedes","Delgado",1,"1939",NULL,NULL,NULL);
-INSERT INTO site_web_mujer VALUES (14,"Clorinda","Matto de Turner",1,"11/11/1852","25/10/1909",NULL,NULL);
-INSERT INTO site_web_mujer VALUES (15,"Adriana","Hiromi Cule",1,NULL,NULL,NULL,NULL);
-INSERT INTO site_web_mujer VALUES (16,"Alfonsina","Barrionuevo",1,NULL,NULL,NULL,NULL);
-INSERT INTO site_web_mujer VALUES (17,"Anael Nuit","Pilares Valdivia",1,NULL,NULL,NULL,NULL);
-INSERT INTO site_web_mujer VALUES (18,"Areli","Aráoz",4,NULL,NULL,NULL,NULL);
-INSERT INTO site_web_mujer VALUES (19,"Emperatriz","Escalante Gutiérrez",1,NULL,NULL,NULL,NULL);
-INSERT INTO site_web_mujer VALUES (20,"Frida","Ibáñez Ayerbe",1,NULL,NULL,NULL,NULL);
-INSERT INTO site_web_mujer VALUES (21,"Gladys","Conde",1,NULL,NULL,NULL,NULL);
-INSERT INTO site_web_mujer VALUES (22,"Nataly","Villena",1,NULL,NULL,NULL,NULL);
-INSERT INTO site_web_mujer VALUES (23,"Nohemi","Estrada Pérez",1,NULL,NULL,NULL,NULL);
-INSERT INTO site_web_mujer VALUES (24,"Tania Consuelo","Gutiérrez Samanez",1,NULL,NULL,NULL,NULL);
+INSERT INTO site_web_mujer(id,nombre,apellido,lugar_id,fecha_nacimiento,fecha_defuncion,trayectoria,link_imagen) VALUES
+	(1,"Iñakapalla","Chávez Bermúdez",1,"20/06/1986",NULL,NULL,"img/not_found.jpg"),
+	(2,"Rosa Augusta","Rivero Ricalde",1,"14/04/1907","01/10/1966",NULL,"img/not_found.jpg"),
+	(3,"Inés Qorich'aska","Quispe Puma",2,"1990",NULL,NULL,"img/not_found.jpg"),
+	(4,"Aleyda","Cárdenas",NULL,"1987",NULL,NULL,"img/not_found.jpg"),
+	(5,"Silvia Rosa","Huamaní Quispe",3,"1982",NULL,NULL,"img/not_found.jpg"),
+	(6,"Gladys","Conde Camargo",1,"1982",NULL,NULL,"img/not_found.jpg"),
+	(7,"Ana María","Milla Hurtado",1,"1977",NULL,NULL,"img/not_found.jpg"),
+	(8,"Ch'aska Eugenia","Anka Ninawaman",3,"1973",NULL,NULL,"img/not_found.jpg"),
+	(9,"Tania","Castro",1,"1973",NULL,NULL,"img/not_found.jpg"),
+	(10,"Karina","Pacheco Medrano",1,"1969",NULL,NULL,"img/not_found.jpg"),
+	(11,"Carmen","Escalante Gutiérrez",1,"1953",NULL,NULL,"img/not_found.jpg"),
+	(12,"Ana Bertha","Vizcarra",1,"1947",NULL,NULL,"img/not_found.jpg"),
+	(13,"Mercedes","Delgado",1,"1939",NULL,NULL,"img/not_found.jpg"),
+	(14,"Clorinda","Matto de Turner",1,"11/11/1852","25/10/1909",NULL,"img/not_found.jpg"),
+	(15,"Adriana","Hiromi Cule",1,NULL,NULL,NULL,"img/not_found.jpg"),
+	(16,"Alfonsina","Barrionuevo",1,NULL,NULL,NULL,"img/not_found.jpg"),
+	(17,"Anael Nuit","Pilares Valdivia",1,NULL,NULL,NULL,"img/not_found.jpg"),
+	(18,"Areli","Aráoz",4,NULL,NULL,NULL,"img/not_found.jpg"),
+	(19,"Emperatriz","Escalante Gutiérrez",1,NULL,NULL,NULL,"img/not_found.jpg"),
+	(20,"Frida","Ibáñez Ayerbe",1,NULL,NULL,NULL,"img/not_found.jpg"),
+	(21,"Gladys","Conde",1,NULL,NULL,NULL,"img/not_found.jpg"),
+	(22,"Nataly","Villena",1,NULL,NULL,NULL,"img/not_found.jpg"),
+	(23,"Nohemi","Estrada Pérez",1,NULL,NULL,NULL,"img/not_found.jpg"),
+	(24,"Tania Consuelo","Gutiérrez Samanez",1,NULL,NULL,NULL,"img/not_found.jpg");
 
-INSERT INTO site_web_publicacion VALUES (1,"Teorías del reflejo","2018",NULL,4);
-INSERT INTO site_web_publicacion VALUES (2,"Ch'askaschay","2004",NULL,8);
-INSERT INTO site_web_publicacion VALUES (3,"T'ika chumpicha","2010",NULL,8);
-INSERT INTO site_web_publicacion VALUES (4,"1 al 20","1970",NULL,12);
-INSERT INTO site_web_publicacion VALUES (5,"Manual del guerillo","1971",NULL,12);
-INSERT INTO site_web_publicacion VALUES (6,"Machupicchu, orgasmo de piedra","2017",NULL,12);
-INSERT INTO site_web_publicacion VALUES (7,"Peces de betún","1969",NULL,13);
-INSERT INTO site_web_publicacion VALUES (8,"Sueño Indigo","2018",NULL,15);
-INSERT INTO site_web_publicacion VALUES (9,"Juego y silencio","2009",NULL,21);
-INSERT INTO site_web_publicacion VALUES (10,"Semillitas","2015",NULL,24);
+INSERT INTO site_web_publicacion(id,titulo,fecha,descripcion,mujer_id) VALUES
+	(1,"Teorías del reflejo","2018",NULL,4),
+	(2,"Ch'askaschay","2004",NULL,8),
+	(3,"T'ika chumpicha","2010",NULL,8),
+	(4,"1 al 20","1970",NULL,12),
+	(5,"Manual del guerillo","1971",NULL,12),
+	(6,"Machupicchu, orgasmo de piedra","2017",NULL,12),
+	(7,"Peces de betún","1969",NULL,13),
+	(8,"Sueño Indigo","2018",NULL,15),
+	(9,"Juego y silencio","2009",NULL,21),
+	(10,"Semillitas","2015",NULL,24);
 
-INSERT INTO site_web_lugar VALUES (1,NULL,NULL,"Cusco",NULL);
-INSERT INTO site_web_lugar VALUES (2,NULL,"Acomayo","Cusco",NULL);
-INSERT INTO site_web_lugar VALUES (3,NULL,"Espinar","Cusco",NULL);
-INSERT INTO site_web_lugar VALUES (4,"Limatambo","Anta","Cusco",NULL);
+INSERT INTO site_web_lugar(id,distrito,provincia,departamento,coordx,coordy) VALUES
+	(1,NULL,NULL,"Cusco",NULL,NULL),
+	(2,NULL,"Acomayo","Cusco",NULL,NULL),
+	(3,NULL,"Espinar","Cusco",NULL,NULL),
+	(4,"Limatambo","Anta","Cusco",NULL,NULL);
 
-INSERT INTO site_web_ejerce VALUES (1,7,"Es");
-INSERT INTO site_web_ejerce VALUES (2,8,"Po");
-INSERT INTO site_web_ejerce VALUES (3,8,"Tr");
-INSERT INTO site_web_ejerce VALUES (4,8,"Pr");
-INSERT INTO site_web_ejerce VALUES (5,10,"Es");
-INSERT INTO site_web_ejerce VALUES (6,10,"Ed");
-INSERT INTO site_web_ejerce VALUES (7,10,"An");
-INSERT INTO site_web_ejerce VALUES (8,14,"Es");
-INSERT INTO site_web_ejerce VALUES (9,14,"Ed");
-INSERT INTO site_web_ejerce VALUES (10,15,"Es");
-INSERT INTO site_web_ejerce VALUES (11,15,"Il");
-INSERT INTO site_web_ejerce VALUES (12,17,"Ed");
-INSERT INTO site_web_ejerce VALUES (13,19,"An");
-INSERT INTO site_web_ejerce VALUES (14,20,"Po");
-INSERT INTO site_web_ejerce VALUES (15,23,"Es");
+INSERT INTO site_web_ejerce(id,mujer_id,categoria_id) VALUES
+	(1,7,"Es"),
+	(2,8,"Po"),
+	(3,8,"Tr"),
+	(4,8,"Pr"),
+	(5,10,"Es"),
+	(6,10,"Ed"),
+	(7,10,"An"),
+	(8,14,"Es"),
+	(9,14,"Ed"),
+	(10,15,"Es"),
+	(11,15,"Il"),
+	(12,17,"Ed"),
+	(13,19,"An"),
+	(14,20,"Po"),
+	(15,23,"Es");
--- escritoras_cusco/excel2sql.py
+++ escritoras_cusco/excel2sql.py
@@ -1,45 +1,101 @@
 import openpyxl
+import argparse
+import os
 
-# Ubicacion del archivo Excel
-excel = '.\\Proposicion de bdd\\bddMujeresDelLibroPeruano.xlsx'
-# Nombre del archivo donde guardar las instrucciones
-sql_out = 'datos.sql'
-# Hojas a leer del archivo Excel
-hojas_nombres = ['Mujer', 'Publicacion', 'Lugar', 'Ejerce']
+parser = argparse.ArgumentParser(description='Analizador de argumentos de script')
+parser.add_argument('--excel', '-x', type=str, nargs='?', help='nombre de archivo excel', default=None)
+parser.add_argument('--output', '-o', type=str, nargs='?', help='nombre de script SQL', default='datos.sql')
+inputArgs = parser.parse_args()
+
+file_sql = open(inputArgs.output, 'w', encoding='utf8')
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+# Si no se especifica un archivo Excel en los argumentos, buscamos uno
+if inputArgs.excel == None:
+    excel_encontrado = False
+    for root, dirs, files in os.walk(dir_path):
+        for file in files:
+            if file.endswith('.xlsx'):
+                excel = str(root) + '\\' + str(file)
+                excel = excel.replace(dir_path + '\\', '')
+                excel_encontrado = True
+                print('Archivo Excel seleccionado automaticamente: ' + excel)
+    if excel_encontrado == False:
+        print('Error: ningun archivo Excel fue encontrado en la presente carpeta')
+        exit(0)
+
+# Si se especifica un archivo Excel en los argumentos, comprobamos que existe
+else:
+    if os.path.isfile(str(inputArgs.excel)):
+        excel = inputArgs.excel
+        print('Archivo Excel seleccionado: ' + excel)
+    else:
+        print('Error: el archivo ' + inputArgs.excel + ' no fue encontrado')
+        excel_encontrado = False
+        for root, dirs, files in os.walk(dir_path):
+            for file in files:
+                if file.endswith('.xlsx'):
+                    excel = str(root) + '\\' + str(file)
+                    excel = excel.replace(dir_path + '\\', '')
+                    excel_encontrado = True
+                    print('Quizas quiso decir ' + excel + ' ?')
+        if excel_encontrado == False:
+            print('Error: ningun archivo Excel fue encontrado en la presente carpeta')
+        exit(0)
 
 workbook = openpyxl.load_workbook(excel)
-file_sql = open(sql_out, 'w', encoding='utf8')
-
-# Borrar los datos existentes en las tablas
-for nombre in hojas_nombres:
-    file_sql.write('DELETE FROM ' + nombre + ';\n')
+#hojas_nombres = ['Mujer', 'Publicacion', 'Lugar', 'Ejerce']
+hojas_nombres = []
+for hoja in workbook:
+    nombre = hoja.title
+    resp = input('Utilizar la hoja ' + nombre + '? (s/n) ')
+    i = 0
+    while resp != 's' and resp != 'n' and i < 5:
+        resp = input('Utilizar la hoja ' + nombre + '? (s/n) ')
+        i += 1
+    if resp == 's':
+        hojas_nombres.append(nombre)
+        file_sql.write('DELETE FROM site_web_' + nombre.lower() + ';\n')
 
 # Iterar en todas las hojas para extraer los datos
 for nombre in hojas_nombres:
     hoja = workbook[nombre]
     id_key = 1
     file_sql.write('\n')
+
+    # Crear query de la forma
+    # INSERT INTO <tabla> (id,...,...,...,...,...)  VALUES
+    query_insert = 'INSERT INTO site_web_' + nombre.lower() + '(id'
+    for row in hoja.iter_rows(min_row=1, max_row=1, min_col=2, max_col=hoja.max_column):
+        for cell in row:
+            query_insert = query_insert + ',' + str(cell.value)
+    query_insert = query_insert + ') VALUES\n'
+    file_sql.write(query_insert)
+    
     for row in hoja.iter_rows(min_row=2, max_row=hoja.max_row, min_col=2, max_col=hoja.max_column):
         # Crear query de la forma
-        # INSERT INTO <tabla> VALUES (id,...,...,...,...,...);
-        query = 'INSERT INTO ' + nombre + ' VALUES (' + str(id_key)
+        # (<id>,...,...,...,...,...),
+        query_value = '\t(' + str(id_key)
         id_key += 1
         for cell in row:
             if cell.value == None:
                 # casilla vacia
-                query = query + ',NULL'
+                query_value = query_value + ',NULL'
             elif cell.data_type == 's':
                 # string (agregar comillas)
-                query = query + ',"' + str(cell.value) + '"'
+                query_value = query_value + ',"' + str(cell.value) + '"'
             elif cell.data_type == 'd':
                 # fecha (agregar comillas y transformar a formato DD/MM/AAAA)
                 fecha = str(cell.value)
-                query = query + ',"' + fecha[8:10] + '/' + fecha[5:7] + '/' + fecha[0:4] + '"'
+                query_value = query_value + ',"' + fecha[8:10] + '/' + fecha[5:7] + '/' + fecha[0:4] + '"'
             elif cell.data_type == 'n' and cell.value > 1500:
                 # anho superior a 1500 (agregar comillas)
                 # ATENCION: pueden haber problemas si se superan las 1500 escritoras
-                query = query + ',"' + str(cell.value) + '"'
+                query_value = query_value + ',"' + str(cell.value) + '"'
             else:
-                query = query + ',' + str(cell.value)
-        query = query + ');\n'
-        file_sql.write(query)
+                query_value = query_value + ',' + str(cell.value)
+        if id_key != hoja.max_row:
+            query_value = query_value + '),\n'
+        else:
+            query_value = query_value + ');\n'
+        file_sql.write(query_value)
