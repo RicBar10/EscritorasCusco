871490082f7bf34787dfc377b14bf50c402eb45d
diegopanepo <diegomv1997@hotmail.com>
2022-04-10 01:18:30 +0200

    nuevo script python para especificar que hojas de excel leer

--- escritoras_cusco/excel2sql.py
+++ escritoras_cusco/excel2sql.py
@@ -2,26 +2,33 @@ import openpyxl
 import argparse
 import os
 
-parser = argparse.ArgumentParser(description='Analizador de argumentos de script')
-parser.add_argument('--excel', '-x', type=str, nargs='?', help='nombre de archivo excel', default=None)
-parser.add_argument('--output', '-o', type=str, nargs='?', help='nombre de script SQL', default='datos.sql')
+parser = argparse.ArgumentParser(description='Generador de comandos SQL a partir de un archivo Excel para una base de datos')
+parser.add_argument('-x', '--excel', type=str, nargs='?', default=None,
+                    help='nombre de archivo Excel, el programa buscará el primer archivo .xlsx en la carpeta si este valor no es especificado')
+parser.add_argument('-o', '--output', type=str, nargs='?', default='datos.sql',
+                    help='nombre de script SQL en salida: no olvidar la extensión ".sql", el nombre por defecto es datos.sql')
+parser.add_argument('-t', '--tablas', type=str, nargs='?', default=None,
+                    help='escribir únicamente la letra inicial de las hojas que se quieren considerar del archivo Excel en un solo string')
 inputArgs = parser.parse_args()
 
 file_sql = open(inputArgs.output, 'w', encoding='utf8')
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 # Si no se especifica un archivo Excel en los argumentos, buscamos uno
+excel_encontrado = False
 if inputArgs.excel == None:
-    excel_encontrado = False
     for root, dirs, files in os.walk(dir_path):
         for file in files:
             if file.endswith('.xlsx'):
                 excel = str(root) + '\\' + str(file)
                 excel = excel.replace(dir_path + '\\', '')
                 excel_encontrado = True
-                print('Archivo Excel seleccionado automaticamente: ' + excel)
-    if excel_encontrado == False:
-        print('Error: ningun archivo Excel fue encontrado en la presente carpeta')
+                print('Archivo Excel seleccionado automáticamente: ' + excel)
+                break
+        if excel_encontrado:
+            break
+    if not excel_encontrado:
+        print('Error: ningún archivo Excel fue encontrado en la actual carpeta.\nAbortando...')
         exit(0)
 
 # Si se especifica un archivo Excel en los argumentos, comprobamos que existe
@@ -30,32 +37,35 @@ else:
         excel = inputArgs.excel
         print('Archivo Excel seleccionado: ' + excel)
     else:
-        print('Error: el archivo ' + inputArgs.excel + ' no fue encontrado')
-        excel_encontrado = False
+        print('Error: el archivo ' + inputArgs.excel + ' no fue encontrado.')
         for root, dirs, files in os.walk(dir_path):
             for file in files:
                 if file.endswith('.xlsx'):
                     excel = str(root) + '\\' + str(file)
                     excel = excel.replace(dir_path + '\\', '')
                     excel_encontrado = True
-                    print('Quizas quiso decir ' + excel + ' ?')
+                    print('¿Quizás quiso decir ' + excel + ' ?')
         if excel_encontrado == False:
-            print('Error: ningun archivo Excel fue encontrado en la presente carpeta')
+            print('Error: ningún archivo Excel fue encontrado en la actual carpeta.')
         exit(0)
 
+# Comparacion de las hojas del Excel con el argumento --tablas para determinar que leer
 workbook = openpyxl.load_workbook(excel)
-#hojas_nombres = ['Mujer', 'Publicacion', 'Lugar', 'Ejerce']
 hojas_nombres = []
-for hoja in workbook:
-    nombre = hoja.title
-    resp = input('Utilizar la hoja ' + nombre + '? (s/n) ')
-    i = 0
-    while resp != 's' and resp != 'n' and i < 5:
-        resp = input('Utilizar la hoja ' + nombre + '? (s/n) ')
-        i += 1
-    if resp == 's':
-        hojas_nombres.append(nombre)
-        file_sql.write('DELETE FROM site_web_' + nombre.lower() + ';\n')
+if inputArgs.tablas != None:
+    for letra in inputArgs.tablas:
+        for hoja in workbook:
+            if letra == hoja.title[0] and hoja.title not in hojas_nombres:
+                hojas_nombres.append(hoja.title)
+                break
+else:
+    hojas_nombres.append('Mujer')
+    print('¿Olvidó el argumento -t? Para más informaciónm use el argumento -h o --help')
+print('Hojas a leer: ' + str(hojas_nombres))
+
+# Borrar los datos existentes en las tablas
+for nombre in hojas_nombres:
+    file_sql.write('DELETE FROM site_web_' + nombre.lower() + ';\n')
 
 # Iterar en todas las hojas para extraer los datos
 for nombre in hojas_nombres:
