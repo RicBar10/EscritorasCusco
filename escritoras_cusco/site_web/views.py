e66b545da3a1be62cb3677c0233f83ca5b3a375a
Lucia Fajardo <luciafalu.2509@gmail.com>
2022-04-18 23:36:07 +0200

    modificacion view y html para mostrar las publicaciones

--- escritoras_cusco/site_web/views.py
+++ escritoras_cusco/site_web/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from django.template import RequestContext
-from .models import Contact
-from site_web.models import Mujer, Ejerce
+from .models import Contact, Publicacion
+from site_web.models import Mujer, Ejerce, Publicacion
 from django.db.models import Q
 
 # views.py
@@ -44,7 +44,8 @@ def galleries(request):
 def gallerie(request, mujer_id):
     template = loader.get_template('singleGallery.html')
     mujer = Mujer.objects.get(id=mujer_id)
-    context = {"mujer": mujer}
+    publicaciones = Publicacion.objects.filter(mujer=mujer_id)
+    context = {"mujer": mujer, "publicaciones": publicaciones}
     return HttpResponse(template.render(context, request=request))
 
 
