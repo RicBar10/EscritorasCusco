5836498e019c637b4314a9a587058860585e6e15
sarastef <sarastefqm@gmail.com>
2022-04-23 16:53:47 +0200

    homePage url & about

--- escritoras_cusco/site_web/urls.py
+++ escritoras_cusco/site_web/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='Index'),
+    path('map', views.map, name='Map'),
     path('galleries', views.galleries, name='Galleries'),
     path('galleries/<int:mujer_id>', views.gallerie, name='Gallerie'),
     path('contact', views.contact, name='Contact'),
